cde80b7b241d45d4cff3a7d999747ddac69d0c09
walidhkhan <walidhkhan@gmail.com>
2021-09-16 14:15:44 -0700

    created api routes for Post model

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -7,6 +7,7 @@ router.get('/', (req, res) => {
     Post.findAll({
         // Query configuration
         attributes: ['id', 'post_url', 'title', 'created_at'],
+        order: [['created_at', 'DESC']],
         include: [
             {
                 model: User,
